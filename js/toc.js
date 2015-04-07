16dbbe3e403a871c2314f62e986b68defd8f7d25
Nan Wang <nantas@gmail.com>
2015-04-07 14:00:15 +0800

    Updated site [ci skip]

---
+++ js/toc.js
@@ -0,0 +1,33 @@
+(function(){
+    'use strict';
+
+    var header = document.getElementById('top');
+    //var container = document.getElementsByTagName('body')[0];
+    var toc = document.getElementById('article-toc');
+    var tocTop = document.getElementById('article-toc-top');
+    var headerHeight = header.clientHeight;
+
+    if (!toc) return;
+
+    function updateSidebarPosition(){
+        //var scrollTop = $('#full-wrap').scrollTop();
+        var scrollTop = $('body').scrollTop();
+        if (scrollTop > headerHeight){
+            toc.classList.add('fixed');
+        } else {
+            toc.classList.remove('fixed');
+        }
+    }
+
+    $(window).scroll( function(){
+        console.log('updating position');
+        window.requestAnimationFrame(updateSidebarPosition);
+    });
+
+    updateSidebarPosition();
+
+    tocTop.addEventListener('click', function(e){
+        e.preventDefault();
+        $('body').scrollTop(0);
+    });
+})();
